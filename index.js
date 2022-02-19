6d1ea7c922185caf001f17a43085ab5c9781081e
Ashish <68690233+asheeeshh@users.noreply.github.com>
2022-02-19 16:55:21 +0530

    update style

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const { Octokit } = require("@octokit/rest");
 const { Base64 } = require("js-base64");
 const fetch = require("node-fetch");
+const { WakaTime } = require('wakatime');
 //const { sha256 } = require("js-sha256").sha256;
 require("dotenv").config();
 
 const {
     GH_TOKEN: githubToken,
+    WK_KEY: wakatimeKey,
 } = process.env;
+
+const client = new WakaTime(`${wakatimeKey}`);
 
 const octokit = new Octokit({
     auth: `token ${githubToken}`,
@@ -16,7 +20,20 @@ async function main() {
     const sp_data = await fetch('https://spotify-np-api.vercel.app/api', {mode: 'cors'});
     const resp = await sp_data.json()
     await constructContent(resp['top'])
-    
+}
+
+async function all_time() {
+    client.stats('all_time').then(data => {
+        return `
+            ${Math.floor(data.data.total_seconds/3600)+150} hr ${Math.floor((data.data.total_seconds%3600)/60)} mins
+        `
+    })
+}
+
+async function today() {
+    client.summaries(new Date()).then(data => {
+        return data.cummulative_total.text
+    })
 }
 
 async function constructContent(response) {
@@ -36,8 +53,12 @@ async function constructContent(response) {
             <a href="">ruby</a> .
             <a href="">go</a></br>
             <a href="${response.url}">top track</a> .
-            <a href="${response.url}">${response.song.toLowerCase()}</a> .
-            <a href="${response.url}">${response.artist.toLowerCase()}</a>
+            <a href="${response.url}">${response.song.toLowerCase().replace(" ", "-")}</a> .
+            <a href="${response.url}">${response.artist.toLowerCase().replace(" ", "-")}</a></br>
+            <a href="">total coding time</a> .
+            <a href="">${await all_time()}</a> .
+            <a href="">today</a> .
+            <a href="">${await today()}</a>
         </samp>
         </p>
 
@@ -48,6 +69,7 @@ async function constructContent(response) {
         repo: "asheeeshh",
         path: "README.md"
     })
+    //console.log(content)
     await updateRepo(Base64.encode(content.replace(/[`\t\n\r]+/g, '').trim()), prevCont.data.sha)
 }
 
